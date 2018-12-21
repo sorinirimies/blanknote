2e142b5eafa64f151a90c7272440556a29b634cb
sorinirimies <mihaiirimies@gmail.com>
2018-12-21 16:37:32 +0100

    added coroutines to viewmodels

--- mobile/src/main/java/ro/sorin/blanknote/ui/notes/NotesViewModel.kt
+++ mobile/src/main/java/ro/sorin/blanknote/ui/notes/NotesViewModel.kt
@@ -1,14 +1,6 @@
 package ro.sorin.blanknote.ui.notes
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ro.sorin.blanknote.model.Note
-
 
 class NotesViewModel : ViewModel() {
-val notes = MutableLiveData<List<Note>>()
-
-    fun getNotes(){
-
-    }
 }
