6249d8d1e69e40d800e8a718aab1f7bf4d9caa0d
sorin <sorin.irimies@moovel.com>
2018-12-21 23:21:13 +0100

    define notes Rv adapter

---
+++ mobile/src/main/java/ro/sorin/blanknote/ui/notes/NotesAdapter.kt
@@ -0,0 +1,59 @@
+package ro.sorin.blanknote.ui.notes
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.listitem_note.view.*
+import ro.sorin.blanknote.R
+import ro.sorin.blanknote.model.Note
+
+
+class NotesAdapter : RecyclerView.Adapter<NotesAdapter.BlankNoteViewHolder>() {
+    private val notes: ArrayList<Note> = ArrayList()
+
+    fun addItem(item: Note) {
+        if (notes.map { it.id }.contains(item.id)) return
+        notes.add(item)
+        notifyItemInserted(notes.indexOf(item))
+    }
+
+    fun addItems(items: List<Note>) {
+        notes.clear()
+        notes.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    fun clearItems() {
+        notes.clear()
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(item: Note) {
+        val position = notes.indexOf(item)
+        notes.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlankNoteViewHolder =
+            BlankNoteViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                            R.layout.listitem_note,
+                            parent,
+                            false))
+
+    override fun onBindViewHolder(holder: BlankNoteViewHolder, position: Int) = holder.bind(notes[position])
+
+    inner class BlankNoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val messageText: TextView = view.tvNoteContent
+        fun bind(note: Note) {
+            messageText.text = note.name
+        }
+    }
+}
+
