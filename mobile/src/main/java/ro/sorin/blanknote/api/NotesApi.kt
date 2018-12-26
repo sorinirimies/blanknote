2a199e853890eb9109e5f70f113b842c9d6478fc
sorinirimies <mihaiirimies@gmail.com>
2018-12-26 22:22:34 +0100

    rename endpoint

--- mobile/src/main/java/ro/sorin/blanknote/api/NotesApi.kt
+++ mobile/src/main/java/ro/sorin/blanknote/api/NotesApi.kt
@@ -3,19 +3,20 @@ package ro.sorin.blanknote.api
 import kotlinx.coroutines.Deferred
 import retrofit2.http.*
 import ro.sorin.blanknote.model.Note
-import ro.sorin.blanknote.model.NoteRequest
+import ro.sorin.blanknote.model.OutOfFoodItem
+import ro.sorin.blanknote.model.OutOfFoodItemRequest
 
 interface NotesApi {
 
-    @GET("todo")
+    @GET("note")
     suspend fun getNotes(@Query("userId") userId: String? = null): Deferred<List<Note>>
 
-    @GET("todo/{id}")
+    @GET("note/{id}")
     suspend fun getNote(@Path("id") id: String): Deferred<Note>
 
-    @POST("todo")
-    suspend fun addNote(@Body note: NoteRequest): Deferred<Note>
+    @POST("note")
+    suspend fun addNote(@Body outOfFoodItem: OutOfFoodItemRequest): Deferred<Note>
 
-    @PUT("todo/{id}")
-    suspend fun updateNote(@Body note: NoteRequest, @Path("id") id: String): Deferred<Note>
+    @PUT("note/{id}")
+    suspend fun updateNote(@Body outOfFoodItem: OutOfFoodItemRequest, @Path("id") id: String): Deferred<Note>
 }
