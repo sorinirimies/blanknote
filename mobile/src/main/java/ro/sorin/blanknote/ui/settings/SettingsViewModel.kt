b831df4a67a117cfa11c7bc73c6aea2aa2edcc11
sorinirimies <mihaiirimies@gmail.com>
2019-01-13 16:40:32 +0100

    optimize imports

--- mobile/src/main/java/ro/sorin/blanknote/ui/settings/SettingsViewModel.kt
+++ mobile/src/main/java/ro/sorin/blanknote/ui/settings/SettingsViewModel.kt
@@ -1,9 +1,5 @@
 package ro.sorin.blanknote.ui.settings
 
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import ro.sorin.blanknote.utils.CoroutineViewModel
 
 class SettingsViewModel : CoroutineViewModel() {
