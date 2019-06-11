b3383cf1de94c3ff3a88bf1bbc93f9eb61e06eaa
sorinirimies <sorin.albu-irimies@porsche.de>
2019-06-11 10:16:46 +0200

    set dispatcher param

--- mobile/src/main/java/ro/sorin/blanknote/utils/CoroutineViewModel.kt
+++ mobile/src/main/java/ro/sorin/blanknote/utils/CoroutineViewModel.kt
@@ -1,14 +1,15 @@
 package ro.sorin.blanknote.utils
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 
-open class CoroutineViewModel : ViewModel(), CoroutineScope {
+open class CoroutineViewModel(dispatcher : CoroutineDispatcher = Dispatchers.IO) : ViewModel(), CoroutineScope {
 
     private val job by lazy(LazyThreadSafetyMode.NONE) { Job() }
-    override val coroutineContext = job + Dispatchers.Main
+    override val coroutineContext = job + dispatcher
 
     override fun onCleared() {
         super.onCleared()
